b5ed96ac8a15633fa52a552f1af22bbccdba29a6
porkostomus <porkostomus@mail.com>
2021-08-13 22:12:27 -0700

    trim unused code

--- src/graph/app.cljs
+++ src/graph/app.cljs
@@ -6,87 +6,9 @@
    [:text {:text-anchor "middle" :x x :y (+ 4 y) :font-family "Monospace"
            :font-size 14} label]])
 
-(defn svg-leaf [label x y]
-  [:g [:rect {:fill "yellow" :stroke "black" :x (- x 14) :y (- y 14) :width 28 :height 28}]
-   [:text {:text-anchor "middle" :x x :y (+ 4 y) :font-family "Monospace"
-           :font-size 14} label]])
-
-(defn undirected-edge
-  "Coordinates are for the text label (edge weight).
-   Path is the edge spline.
-   Polygon points are for the arrows."
-  [path]
-  [:g
-   [:path {:fill "none" :stroke "black" :d path}]])
-
-
-(defn svg-edge
-  "Coordinates are for the text label (edge weight).
-   Path is the edge spline.
-   Polygon points are for the arrows."
-  [label x y path points]
-  [:g
-   [:path {:fill "none" :stroke "black" :d path}]
-   [:polygon {:fill "black" :stroke "black" :points points}]
-   [:text {:text-anchor "middle" :x x :y y :font-family "Monospace" :font-size 14.00} label]])
-
-(defn g []
-  {:1 {:2 1 :3 2}
-   :2 {:4 3 :5 4}
-   :3 {:6 5 :7 6}
-   :4 {}
-   :5 {}
-   :6 {}
-   :7 {}})
-
-(def g3
-  {:1 {:2 1 :3 2}
-   :2 {:4 3 :5 4}
-   :3 {:6 5 :7 6}
-   :4 {:8 1 :9 1}
-   :5 {:10 1 :11 1}
-   :6 {:12 1 :13 1}
-   :7 {:14 1 :15 1}
-   :8 {}
-   :9 {}
-   :10 {}
-   :11 {}
-   :12 {}
-   :13 {}
-   :14 {}
-   :15 {}})
-
-#_(defn parent [node tree]
-    (first (filter #(contains? (get tree %) node) (keys tree))))
-
-;(parent :2 g3)
-
-#_(defn node-depth [node tree]
-    (loop [n node parents []]
-      (if (nil? (parent n tree))
-        (count parents)
-        (recur (parent n tree) (conj parents (parent n tree))))))
-
-;(node-depth :8 g3)
-
-#_(defn tree-height [tree]
-    (inc (apply max (map #(node-depth % tree) (keys tree)))))
-
-;(tree-height g3)
-
-(defn leaf-nodes [tree]
-  (filter #(empty? (get tree %)) (keys tree)))
-
-(count (leaf-nodes g3))
-
-(.-innerWidth js/window)
-(.-innerHeight js/window)
-
-(filter (fn [[node children]] (seq children)) g3)
-
 (defonce my-nodes (r/atom (vec (range 1 3))))
 
-(reset! my-nodes (vec (range 1 119)))
+(reset! my-nodes (vec (range 1 5)))
 
 (defn tree-height
   "Returns the number of levels high a binary heap
@@ -106,8 +28,6 @@
    in a binary tree or heap."
   [n]
   (inc (mod n (tree-width n))))
-
-(/ 566 8)
 
 (defn node-x
   "Returns the x-coordinate of node n."
@@ -149,8 +69,6 @@
   (for [child (range 1 (count nodes))]
     [edge (get node-locs (.ceil js/Math (- (/ child 2) 1)))
      (get node-locs child)]))
-
-(tree-height (count @my-nodes))
 
 (defn heap []
   [:svg {:width "100%"
